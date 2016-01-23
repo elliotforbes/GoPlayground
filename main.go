6b34d2b75f9fc23b6af526552a6d82045b2bb4d3
emforce <emforce@live.com>
2016-01-23 17:49:32 +0000

    updated http router

--- main.go
+++ main.go
@@ -35,6 +35,10 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
+    
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
 
